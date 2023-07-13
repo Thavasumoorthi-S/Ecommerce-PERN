5df4fcd58be5fb56021cf08563d6c4e5412f8a66
Thavasumoorthi-S <123466522+Thavasumoorthi-S@users.noreply.github.com>
2023-07-14 01:37:12 +0530

    Update db.js

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,4 +1,4 @@
 const pg = require('pg-promise')();
-const connection = 'postgres://postgres:Thava2001@@localhost:5432/Ecommerce';
+const connection = 'postgres://postgres:password@localhost:5432/Ecommerce';
 const db = pg(connection);
 module.exports = db;
